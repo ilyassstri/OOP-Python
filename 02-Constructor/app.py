6b21939e2c8039607f9043f2a2f6f58c3bb3021c
trikhaqiqi <trikhaqiqi@gmail.com>
2023-10-29 14:04:44 +0700

    class

--- 02-Constructor/app.py
+++ 02-Constructor/app.py
@@ -19,35 +19,61 @@ dengan instance atau instansiasi
 # ilyas, otong
 # mempunyai atribut name, email, role
 
-class User:
-    def __init__(self, name, email, role):
+# class User:
+#     def __init__(self, name, email, role):
+#         self.name = name
+#         self.email = email
+#         self.role = role
+
+# ilyas = User("ilyas", "trikhaqiqi@gmail.com", "User")
+# otong = User("otong", "otong@gmail.com", "admin")
+
+# print(otong.name)
+# print(otong.email)
+# print(otong.role)
+
+
+# class Mahasiswa:
+#     #initializer
+#     def __init__(self):
+#         self.nim = "M0501001"
+#         self.nama = "Uzumaki Saburo"
+#         self.alamat = "Konohagakure"
+
+#     # menampilkan isi atribut
+#     def printMhs(self):
+#         print(self.nim)
+#         print(self.nama)
+#         print(self.alamat)
+
+# # membuat object dari class Mahasiswa
+# objMhs = Mahasiswa()
+
+# # memanggil method printMhs 
+# objMhs.printMhs()
+
+""" Contoh 3  | Cara define langsung"""
+class Programmer:
+    def __init__(self):
+        self.name = "ilyas"
+        self.role = "developer"
+        self.email = "trikhaqiqi@gmail.com"
+
+    # kalau ingin menampilkannya secara langsung
+    def printProgrammer(self):
+        print(self.name)
+        print(self.role)
+        print(self.email)
+
+objectProgrammer = Programmer()
+objectProgrammer.printProgrammer()
+
+""" Contoh 4 | cara define diakhir """
+class ITSupport:
+    def __init__(self, name, role):
         self.name = name
-        self.email = email
         self.role = role
 
-ilyas = User("ilyas", "trikhaqiqi@gmail.com", "User")
-otong = User("otong", "otong@gmail.com", "admin")
-
-print(otong.name)
-print(otong.email)
-print(otong.role)
-
-
-class Mahasiswa:
-    #initializer
-    def __init__(self):
-        self.nim = "M0501001"
-        self.nama = "Uzumaki Saburo"
-        self.alamat = "Konohagakure"
-
-    # menampilkan isi atribut
-    def printMhs(self):
-        print(self.nim)
-        print(self.nama)
-        print(self.alamat)
-
-# membuat object dari class Mahasiswa
-objMhs = Mahasiswa()
-
-# memanggil method printMhs 
-objMhs.printMhs()
+pegawai = ITSupport("irln", "IT Support")
+print(pegawai.name)
+print(pegawai.role)
