25f6cbe5fb5a77cd5979c031af2f888184e15287
trikhaqii <trikhaqii@gmail.com>
2023-11-05 21:43:53 +0700

    ok

--- 13-InnerFunction/app.py
+++ 13-InnerFunction/app.py
@@ -1,24 +1,24 @@
 # Innner Function -> adalah fungsi/method yang berada di dalam method
 
 """ Contoh 1 """
-class Game:
-    def __init__(self, title, price):
-        self.title = title
-        self.price = price
+# class Game:
+#     def __init__(self, title, price):
+#         self.title = title
+#         self.price = price
 
-    def info(self):
-        def print_title():
-            print(f"Title : {self.title}")
+#     def info(self):
+#         def print_title():
+#             print(f"Title : {self.title}")
         
-        def print_price():   
-            print(f"Price : {self.price}")
+#         def print_price():   
+#             print(f"Price : {self.price}")
 
-        print_title()
-        print_price()
+#         print_title()
+#         print_price()
         
 
-ilyas = Game("Cyberpunk", 120)
-ilyas.info()
+# ilyas = Game("Cyberpunk", 120)
+# ilyas.info()
 
 """ Contoh 2 """
 # class Game:
@@ -39,3 +39,16 @@ ilyas.info()
 # title = ilyas.info()
 # print(title)
 # print(title())
+
+class Super:
+    def __init__(self, name):
+        self.name = name
+
+    def info(self):
+        def print_name():
+            return f"Name : {self.name}"
+        
+        return print_name
+    
+test = Super("Batman")
+print(test)
